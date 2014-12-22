534907e80bdfe385594ec378718e944511e00b20
Sergey <sokolcjrjk@gmail.com>
2014-12-22 14:10:59 +0300

    Empty UI

---
+++ Code/Jarboo.Admin.Web/App_Start/BundleConfig.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace Jarboo.Admin.Web.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/js/jquery").Include(
+                "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/js/bootstrap").Include(
+                "~/Scripts/bootstrap.js"));
+
+
+
+            bundles.Add(new StyleBundle("~/css/site").Include(
+                "~/Content/main.css"));
+
+            bundles.Add(new StyleBundle("~/css/bootstrap").Include(
+                "~/Content/bootstrap.css"));
+        }
+    }
+}
--- Code/Jarboo.Admin.Web/Global.asax.cs
+++ Code/Jarboo.Admin.Web/Global.asax.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
+
+using Jarboo.Admin.Web.App_Start;
 
 namespace Jarboo.Admin.Web
 {
@@ -12,7 +15,9 @@ namespace Jarboo.Admin.Web
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
