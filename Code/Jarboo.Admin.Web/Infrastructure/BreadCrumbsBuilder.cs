8956bf75a7b190a4c319a785507526b38c3f0cd5
Sergey <sokolcjrjk@gmail.com>
2014-12-25 17:34:25 +0300

    Bread crumbs helper

---
+++ Code/Jarboo.Admin.Web/Infrastructure/BreadCrumbsBuilder.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Jarboo.Admin.Web.Infrastructure
+{
+    public class BreadCrumbsBuilder
+    {
+        private StringBuilder stringBuilder;
+
+        public BreadCrumbsBuilder()
+        {
+            stringBuilder = new StringBuilder();
+            stringBuilder.Append("<div class='breadcrumbs small'>");
+        }
+
+        public BreadCrumbsBuilder Add(string url, string label)
+        {
+            stringBuilder.Append(string.Format(@"<span class='item'>
+                    <strong>
+                        <a href='{0}'>{1}</a>
+                    </strong>
+                </span>", url, label));
+            return this;
+        }
+        public BreadCrumbsBuilder Add(string label)
+        {
+            stringBuilder.Append(string.Format("<span class='item'>{0}</span>", label));
+            return this;
+        }
+
+        public MvcHtmlString Done()
+        {
+            stringBuilder.Append("</div>");
+            return MvcHtmlString.Create(stringBuilder.ToString());
+        }
+
+        public static BreadCrumbsBuilder Start()
+        {
+            return new BreadCrumbsBuilder();
+        }
+    }
+}
